a357e543c913736bebf128fe25416dbb571f9401
dmcquiggin@mcraeimaging.com <dmcquiggin@mcraeimaging.com>
2023-03-03 10:03:29 -0500

    Added PUT(Update) Method

--- server/api/recipe/[id].js
+++ server/api/recipe/[id].js
@@ -44,6 +44,33 @@ export default defineEventHandler(async (event) => {
       return {body};
     }
 
+    //EDIT RECIPE METHOD
+    if (event.req.method === "PUT")
+    {
+      const body = await readBody(event);
+
+      //Get the information to be written to the database
+      const {name} = await readBody(event);
+      const {description} = await readBody(event);
+      const {instructions} = await readBody(event);
+      const {ingredients} = await readBody(event);
+      const {image} = await readBody(event);
+      
+      //Write to the database
+      await prisma.recipes.create({
+        data: {
+          name: name,
+          description: description,
+          instructions: instructions,
+          image: image,
+          v: 0,
+          ingredients: ingredients
+        },
+      })
+
+      return {body};
+    }
+
     //GET SINGLE RECIPE FOR VIEWING METHOD
   if (event.req.method === "DELELE")
     {
