69773c85ed073432fa15c90384a2f1f959d51b1b
dmcquiggin@mcraeimaging.com <dmcquiggin@mcraeimaging.com>
2023-03-10 13:20:27 -0500

    New Page for Create and Edit

---
+++ pages/recipe/form/[action]-[[id]].vue
@@ -0,0 +1,15 @@
+<script setup>
+    const route = useRoute();
+    const action = route.params.action;
+    const id = route.params.id;
+
+    const {data: recipe} = await useFetch('/api/recipe/' + id);
+</script>
+
+<template>
+    <div>
+        <h2>action: {{ action }}</h2><br>
+        <h2>id: {{ id }}</h2>
+        <h2 v-if="recipe != null">recipe.name: {{ recipe.name }}</h2>
+    </div>
+</template>
